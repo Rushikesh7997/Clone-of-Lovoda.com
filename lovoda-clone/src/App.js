f0a74cf83273a6f1d0f4d219655f8b81429e7f66
Rushikesh7997 <pardhikar26@gmail.com>
2022-10-01 15:37:30 +0530

    project upload

---
+++ lovoda-clone/src/App.js
@@ -0,0 +1,21 @@
+
+import MainRoutes from './Routes/MainRoutes';
+import Navbar from './Components/Header/Navbar'
+import Footer from './Components/footer/footer';
+import OrderComplete from './Pages/Alok/OrderComplete';
+
+
+function App() {
+  return (
+    <div>
+       <Navbar/> 
+      <MainRoutes/>
+      <Footer/> 
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ lovoda-clone/src/Routes/MainRoutes.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import { HomePage } from '../Pages/Rushikesh/HomePage'
+import AllPages from '../Components/ShopAll/AllPages'
+import NewProducts from '../Pages/Krishna/NewProducts'
+import CartPage from '../Pages/Swati/CartPage'
+import PaymentPage from '../Pages/Swati/PaymentPage'
+import RequiredAuth from '../Components/Authentication/RequiredAuth'
+import PaymentSuccess from '../Pages/Alok/PaymentSuccess'
+import Signin from '../Components/Auth/signin'
+import ProductDetails from '../Pages/Krishna/ProductDetails'
+import OrderComplete from '../Pages/Alok/OrderComplete'
+import ShippingPage from '../Pages/Swati/ShippingPage'
+import PaymentMain from '../Pages/Swati/PaymentMain'
+
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/:id' element={<ProductDetails/>} />
+
+        <Route path='/new_products' element={<NewProducts/>}/>
+        <Route path='/new_products/:id' element={<ProductDetails/>}/>
+
+        <Route path='/all_products' element={<AllPages/>}/>
+        
+        <Route path='/all_products/:id' element={<ProductDetails/>}/>
+        <Route path='/cart' element={<CartPage/>}/>
+        <Route path='/login' element={<Signin/>}/>
+        <Route path='/cart/information' element={
+            <RequiredAuth>
+        <PaymentPage/>
+            </RequiredAuth>
+        }/>
+        <Route path='/cart/information/shipping' element={
+            <RequiredAuth>
+        <ShippingPage/>
+            </RequiredAuth>
+        }/>
+        <Route path='/cart/information/shipping/payment' element={
+            <RequiredAuth>
+        <PaymentSuccess/>
+        {/* <PaymentMain/> */}
+            </RequiredAuth>
+        }/>
+        <Route path='/cart/information/shipping/payment/order_successful' element={
+            <RequiredAuth>
+        <OrderComplete/>
+            </RequiredAuth>
+        }/>
+    </Routes>
+  )
+}
+
+export default MainRoutes
+  
